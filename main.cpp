e549bc420f2ba8a8f16da80b30b5bdb7bef902e6
Josh <josh.e.stein@gmail.com>
2020-03-20 17:47:52 +0200

    Find circle contours using background subtraction

--- main.cpp
+++ main.cpp
@@ -7,71 +7,107 @@
 
 using namespace cv;
 
-Point find_closest_previous_center(Point &p, std::vector<Point> &prev_centers);
-double euclidean_distance(Point &p1, Point &p2);
+Point find_closest_previous_center(Point2f &p, std::vector<Point2f> &prev_centers);
+double euclidean_distance(Point2f &p1, Point2f &p2);
 
 enum direction {upwards, downwards};
 
 int main(int argc, char* argv[]) {
     VideoCapture cap(0);
-    std::vector<Point> prev_centers;
-    direction prev_dir;
+    std::vector<Point2f> prev_centers;
+    direction prev_dir = upwards;
     int throws = 0;
+    Mat prev_upper_third;
 
     while(true) {
-        Mat frame, upper_third, upper_third_grey;
+        Mat frame, upper_third, frame_delta, processed_frame;
         cap >> frame;
 
         upper_third = frame(Rect(0, 0, frame.cols, frame.rows/2));
 
         // Convert to gray
-        cvtColor(upper_third, upper_third_grey, COLOR_BGR2GRAY);
+        cvtColor(upper_third, upper_third, COLOR_BGR2GRAY);
 
         // Reduce the noise so we avoid false circle detection
-        GaussianBlur(upper_third_grey, upper_third_grey, Size(9, 9), 2, 2);
+        GaussianBlur(upper_third, upper_third, Size(9, 9), 2, 2);
+        if (prev_upper_third.empty()) {
+            prev_upper_third = upper_third;
+        } 
 
-        std::vector<Vec3f> circles;
-        std::vector<Point> centers;
-        direction dir;
+        // get difference between frames (i.e. moving balls)
+        absdiff(upper_third, prev_upper_third, frame_delta);
+        threshold(frame_delta, processed_frame, 25, 255, THRESH_BINARY);
 
-        HoughCircles(upper_third_grey, circles, HOUGH_GRADIENT, 1, upper_third_grey.rows/8, 100, 22, 0, 0);
-        if (circles.size() == 0) {
-            centers.clear();
+        prev_upper_third = upper_third;
+
+        // erode and dilate to remove impurities
+        Mat element = getStructuringElement(MORPH_ELLIPSE, Size(9, 9), Point(-1, -1));
+        erode(processed_frame, processed_frame, element, Point(-1, -1), 2);
+        dilate(processed_frame, processed_frame, element, Point(-1, -1), 2);
+
+        // find contours
+        std::vector<std::vector<Point> > contours;
+        Mat processed_frame_copy = processed_frame.clone();  // prevent over-writing original data
+        findContours(processed_frame_copy, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        if (contours.size() == 0) {
+            prev_centers.clear();
             prev_dir = upwards;
+            imshow("Frame", frame);
+            if (waitKey(10) == 27) break;
+            continue;
         }
+
+        // find largest circle
+        double largest_area = 0.0;
+        int contour_idx = 0;
+        for (int i = 0; i < contours.size(); ++i) {
+            double area = contourArea(contours[i]);
+            if (area > largest_area) {
+                largest_area = area;
+                contour_idx = i;
+            } 
+        }
+
+        // std::vector<std::vector<Point> > contours_poly(contours.size());
+        // std::vector<Point2f> centers(contours.size());
+        // std::vector<float> radius(contours.size());
+
+        std::vector<Point> contours_poly;
+        Point2f center;
+        float radius;
+
+        approxPolyDP(contours[contour_idx], contours_poly, 3, true);
+        minEnclosingCircle(contours_poly, center, radius);
+        // Point2f center = centers[i];
+        // Scalar color(rand()&255, rand()&255, rand()&255);
+        // drawContours(drawing, contours, i, color);
         
-        for( size_t i = 0; i < circles.size(); ++i) {
-            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+        // circle center
+        circle(frame, center, 3, Scalar(0,255,0), -1, 8, 0);
+        // circle outline
+        circle(frame, center, radius, Scalar(0,0,255), 3, 8, 0);
 
-            centers.push_back(center);
+        direction dir;
+        if (prev_centers.empty()) {
+            prev_centers.push_back(center);
+        } else {
+            Point prev_center = find_closest_previous_center(center, prev_centers);
 
-            if (prev_centers.empty()) {
-                prev_centers.push_back(center);
-                prev_dir = upwards;
-            } else {
-                Point prev_center = find_closest_previous_center(center, prev_centers);
-
-                // row-major order
-                if (prev_center.y > center.y) {  // upwards
-                    dir = upwards;
-                } else {  // downwards
-                    dir = downwards;
-                }
-
-                if (prev_dir == upwards && dir == downwards) {
-                    throws += 1;
-                    std::cout << throws << std::endl;
-                }
-                prev_dir = dir;
+            // row-major order
+            if (prev_center.y > center.y) {  // upwards
+                dir = upwards;
+            } else {  // downwards
+                dir = downwards;
             }
 
-            int radius = cvRound(circles[i][2]);
-            // circle center
-            circle( frame, center, 3, Scalar(0,255,0), -1, 8, 0 );
-            // circle outline
-            circle( frame, center, radius, Scalar(0,0,255), 3, 8, 0 );
+            if (prev_dir == upwards && dir == downwards) {
+                throws += 1;
+                std::cout << throws << std::endl;
+            }
+            prev_dir = dir;
+            
         }
-
         imshow("Frame", frame);
         if (waitKey(10) == 27) break;
     }
@@ -81,7 +117,7 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-Point find_closest_previous_center(Point &p, std::vector<Point> &prev_centers) {
+Point find_closest_previous_center(Point2f &p, std::vector<Point2f> &prev_centers) {
     double min_dist = std::numeric_limits<double>::max();
     Point closest_point;
     for (auto iter = prev_centers.begin(); iter != prev_centers.end(); ++iter) {
@@ -94,7 +130,7 @@ Point find_closest_previous_center(Point &p, std::vector<Point> &prev_centers) {
     return closest_point;
 }
 
-double euclidean_distance(Point &p1, Point &p2) {
+double euclidean_distance(Point2f &p1, Point2f &p2) {
     double d_x = p1.x - p2.x;
     double d_y = p1.y - p2.y;
 
